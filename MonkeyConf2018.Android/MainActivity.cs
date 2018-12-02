574f9c6e830121672a9b980de2e09bf8b2eee490
jorgemhtdev <jorge@jorgemht.dev>
2018-12-02 18:44:22 +0100

    [Added] - Media Camera [Added] - Views, ViewModles and Services [Added] - AppSettings

--- MonkeyConf2018.Android/MainActivity.cs
+++ MonkeyConf2018.Android/MainActivity.cs
@@ -1,14 +1,11 @@
-﻿using System;
-
-using Android.App;
-using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
-namespace MonkeyConf2018.Droid
+﻿namespace MonkeyConf2018.Droid
 {
+    using Android.App;
+    using Android.Content.PM;
+    using Android.OS;
+    using Plugin.CurrentActivity;
+    using Xamarin.Forms;
+
     [Activity(Label = "MonkeyConf2018", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
@@ -17,9 +14,15 @@ namespace MonkeyConf2018.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+        {
+            Plugin.Permissions.PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
---
+++ MonkeyConf2018/Views/HotDotView.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MonkeyConf2018.Views
+{
+    using MonkeyConf2018.ViewModels;
+    using Xamarin.Forms;
+
+    public partial class HotDotView : ContentPage
+    {
+        public HotDotView()
+        {
+            InitializeComponent();
+            BindingContext = new HotDogViewModel();
+        }
+    }
+}
